835db29585be8c78e527bcdd6a214e8cdfcd538a
Dan Drinkard <dan.drinkard@gmail.com>
2013-08-05 13:06:33 -0400

    oops config is a dict

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ import json
 from solver import choose, rank
 from flask import Flask
 app = Flask(__name__)
-app.config.DEBUG = True if os.environ.get('DEBUG', False) == 'True' else False
+app.config['DEBUG'] = True if os.environ.get('DEBUG', False) == 'True' else False
 
 
 @app.route('/choose.json')
